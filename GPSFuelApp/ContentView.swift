0baf0403193e6b5899b8f1abf707cef8c100aff0
Vitor Lopes Leonardi <vitor.leonardi@theiconic.com.au>
2019-10-23 11:16:53 +1100

    Initial Commit

---
+++ GPSFuelApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GPSFuelApp
+//
+//  Created by Vitor Leonardi on 23/10/19.
+//  Copyright © 2019 Vitor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
